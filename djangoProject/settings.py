3e1be39be5018a5fd95c745a899edb7b5ab078c9
pujanrajrai <pujanr0809@gmail.com>
2021-09-30 14:09:23 +0545

    static files check

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -119,6 +119,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
